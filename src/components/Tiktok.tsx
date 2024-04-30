e88e89de4f6633dbe15b250014220ce90f2a79c1
sarangsayang <admin@sarangsayang.com>
2024-04-30 20:38:53 +0800

    tiktok mobile

--- src/components/Tiktok.tsx
+++ src/components/Tiktok.tsx
@@ -9,17 +9,17 @@ const Tiktok = () => {
           <iframe
             src="https://www.tiktok.com/embed/7340614540448550152"
             allow="encrypted-media;"
-            className="w-[33%] h-full border-0 bg-transparent"
+            className="w-full md:w-[50%] lg:w-[33%] h-full border-0 bg-transparent"
           ></iframe>
           <iframe
             src="https://www.tiktok.com/embed/7345821745871326482"
             allow="encrypted-media;"
-            className="w-[33%] h-full border-0 bg-transparent"
+            className="w-[50%] lg:w-[33%] h-full border-0 bg-transparent hidden md:block"
           ></iframe>
           <iframe
             src="https://www.tiktok.com/embed/7351017804654497032"
             allow="encrypted-media;"
-            className="w-[33%] h-full border-0 bg-transparent"
+            className="w-[33%] h-full border-0 bg-transparent hidden lg:block"
           ></iframe>
         </div>
       </MaxWidthWrapper>
