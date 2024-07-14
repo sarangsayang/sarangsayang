e44752897f3a4357e72298d28252e22c08df3f39
sarangsayang <admin@sarangsayang.com>
2024-07-14 14:04:39 +0800

    gg tiktok

--- src/components/Tiktok.tsx
+++ src/components/Tiktok.tsx
@@ -7,17 +7,17 @@ const Tiktok = () => {
       <MaxWidthWrapper>
         <div className="w-full h-[800px] flex flex-row items-center">
           <iframe
-            src="https://www.tiktok.com/embed/7371436905285668104"
+            src="https://www.tiktok.com/embed/7390336091846348050"
             allow="encrypted-media;"
             className="w-full md:w-[50%] lg:w-[33%] h-full border-0 bg-transparent"
           ></iframe>
           <iframe
-            src="https://www.tiktok.com/embed/7345821745871326482"
+            src="https://www.tiktok.com/embed/7382878868832521480"
             allow="encrypted-media;"
             className="w-[50%] lg:w-[33%] h-full border-0 bg-transparent hidden md:block"
           ></iframe>
           <iframe
-            src="https://www.tiktok.com/embed/7382878868832521480"
+            src="https://www.tiktok.com/embed/7371436905285668104"
             allow="encrypted-media;"
             className="w-[33%] h-full border-0 bg-transparent hidden lg:block"
           ></iframe>
