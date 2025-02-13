8a4748892f3eb43f71f53d6e290afc7ec89dc774
haiieyes <admin@sarangsayang.com>
2025-02-13 14:23:37 +0800

    tiktok vids change

--- src/components/Tiktok.tsx
+++ src/components/Tiktok.tsx
@@ -7,17 +7,17 @@ const Tiktok = () => {
       <MaxWidthWrapper>
         <div className="w-full h-[800px] flex flex-row items-center">
           <iframe
-            src="https://www.tiktok.com/embed/7450083497194163463"
+            src="https://www.tiktok.com/embed/7459730809088412935"
             allow="encrypted-media;"
             className="w-full md:w-[50%] lg:w-[33%] h-full border-0 bg-transparent"
           ></iframe>
           <iframe
-            src="https://www.tiktok.com/embed/7402588471920954632"
+            src="https://www.tiktok.com/embed/7464931555450899719"
             allow="encrypted-media;"
             className="w-[50%] lg:w-[33%] h-full border-0 bg-transparent hidden md:block"
           ></iframe>
           <iframe
-            src="https://www.tiktok.com/embed/7444889029419502855"
+            src="https://www.tiktok.com/embed/7460475998874701063"
             allow="encrypted-media;"
             className="w-[33%] h-full border-0 bg-transparent hidden lg:block"
           ></iframe>
